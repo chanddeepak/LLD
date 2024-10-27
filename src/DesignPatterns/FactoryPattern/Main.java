aed3689aeba8f9353ecadc2da4ebbd22a2a508c0
Deepak Chand <deepakchand@Deepaks-MacBook-Air.local>
2024-10-27 20:09:51 +0530

    Implemented factory pattern

---
+++ src/DesignPatterns/FactoryPattern/Main.java
@@ -0,0 +1,10 @@
+package DesignPatterns.FactoryPattern;
+
+public class Main {
+    public static void main(String args[]) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        shapeFactory.getShape("CIRCLE").draw();
+        shapeFactory.getShape("RECTANGLE").draw();
+        shapeFactory.getShape("SQUARE").draw();
+    }
+}
